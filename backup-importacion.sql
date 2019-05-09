bc275dde56baf11c0abbd7324015be7a696a71a6
ErickHR <Erick08_999@hotmail.com>
2019-05-09 01:02:08 -0500

    backup importacion

---
+++ backup-importacion.sql
@@ -0,0 +1,12 @@
+/*
+    primero se debe crear un usuario https://github.com/ErickHR/Oracle-SQL/blob/master/crear_usuario.sql
+    segundo abra una terminal y coloque lo siguiente
+    
+    IMP nuevo_usuario/contraseña FULL = Y FILE = direccion_donde_se_encuentra_el_documento\nombre_del_documento.DMP
+    
+    el nombre_del_documento es el nombre que se le asigno al momento de hacer la exportacion
+    
+            exportacion-->https://github.com/ErickHR/Oracle-SQL/blob/master/crear_usuario.sql
+*/
+
+IMP ISAAC/123 FULL = Y FILE = C:\Users\PROPIETARIO\Desktop\BD_ERICK.DMP

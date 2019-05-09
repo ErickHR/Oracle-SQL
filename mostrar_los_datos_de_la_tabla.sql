3923fb6cbb29674a03a511f3215e897bfb92cd70
ErickHR <Erick08_999@hotmail.com>
2019-05-08 20:13:13 -0500

    mostrar datos de la tabla

---
+++ mostrar_los_datos_de_la_tabla.sql
@@ -0,0 +1,20 @@
+/*
+    para mostrar todo los campos de la tabla
+       SELECT * FROM nombre_de_la_tabla; 
+   
+   para mostrar solo un campo de la tabla
+   
+        SELECT campo_de_la_tabla_que_se_quiere_mostrar FROM nombre_de_la_tabla;
+   
+   
+   para mostrar dos o mas campos de la tabla se separa con una coma (,)
+    
+       SELECT campo_de_la_tabla, campo_de_la_tabla FROM nombre_de_la_tabla;
+   
+*/
+
+SELECT * FROM PERSONA;
+
+SELECT NOMBRE FROM PERSONA;
+
+SELECT NOMBRE, EDAD FROM PERSONA;

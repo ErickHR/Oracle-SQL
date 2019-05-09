7693032442726ddaf432251a13883c612150954a
ErickHR <Erick08_999@hotmail.com>
2019-05-09 01:26:57 -0500

    creando un servidor

---
+++ creando_servidor.sql
@@ -0,0 +1,13 @@
+/*
+    desde una PC1 cree un usuario https://github.com/ErickHR/Oracle-SQL/blob/master/crear_usuario.sql
+    
+    y desde otra PC2 
+        en la terminal escriba
+        
+        SQLPLUS usuario_de_PC1/contraseña@direccion_IP_de_PC1
+        
+    NOTA:
+        para saber una ip de una pc, escriba en la terminal ipconfig
+*/
+
+SQLPLUS ERICK/123@192.168.0.0

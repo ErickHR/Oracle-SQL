fc3eb6786800dd164c54dc96ff99039bd74af9ce
ErickHR <Erick08_999@hotmail.com>
2019-05-08 23:15:58 -0500

    modificando datos de la tabla

---
+++ modificar_datos_tabla.sql
@@ -0,0 +1,12 @@
+/*
+    UPDATE nombre_de_la_tabla
+    SET 
+    NOMBRE = 'Isaac' --> campo que se quiere modificar 
+    WHERE 
+    IDPERSONA = 1; -->ingrese el id de la fila que se quiere modificar
+*/
+UPDATE PERSONA
+SET 
+NOMBRE = 'Isaac'
+WHERE 
+IDPERSONA = 1;

19b97da32bb909e5272522be89d928992a325c8f
ErickHR <Erick08_999@hotmail.com>
2019-05-09 00:22:08 -0500

    exportar

---
+++ backup.sql
@@ -0,0 +1,13 @@
+/*
+    
+    abra una terminal y escriba lo siguiente
+    
+        EXP nombre_del_usuario/contraseña FULL=Y FILE=donde_quiere_guardar_el_archivo/nombre_que_le_asigna_al_archivo.dmp
+    
+*/
+
+EXP ERICK/123 FULL=Y FILE=C:\Users\PROPIETARIO\Desktop/BD_ERICK.DMP
+
+
+
+
